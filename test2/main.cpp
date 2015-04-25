cd8d31bd860354cc4e8dd2d0554d43da7e43dda2
rslobjects <richardlee@Richards-MBP.home>
2015-04-25 11:44:54 -0400

    test

---
+++ test2/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  test2
+//
+//  Created by Richard Lee on 12/7/14.
+//  Copyright (c) 2014 Richard Lee. All rights reserved.
+//
+
+#include <iostream>
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
